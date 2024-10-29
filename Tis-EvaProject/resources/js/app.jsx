62a3bc7118f6a99b9004d9ced045d61bbd7529c0
Franco <franco.sahonero.figueroa@gmail.com>
2024-10-29 07:56:44 -0400

    arreglos

--- Tis-EvaProject/resources/js/app.jsx
+++ Tis-EvaProject/resources/js/app.jsx
@@ -4,18 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Pages/Login';
 import Proyectos from './Pages/Proyectos';
 import HomePage from './Pages/HomePage';
-import Register from './Pages/Register';  // Importamos Register
-import ForgotPassword from './Pages/ForgotPassword';  // Importamos ForgotPassword
-import ProyectoEstudiante from  './Pages/ProyectoEstudiante';   // Importamos ProyectoEstudiante
-import PlanificacionEstudiante from  './Pages/PlanificacionEstudiante';  // Importamos PlanificacionEstudiante
-import HistoriaUsuario from   './Pages/HistoriaUsuario';  // Importamos HistoriaUsuario
+import Register from './Pages/Register';
+import ForgotPassword from './Pages/ForgotPassword';
+import ProyectoEstudiante from './Pages/ProyectoEstudiante';
+import PlanificacionEstudiante from './Pages/PlanificacionEstudiante';
+import HistoriaUsuario from './Pages/HistoriaUsuario';
 import Perfil from "./Pages/Perfil";
-<<<<<<< HEAD
 import ApproveAccounts from './Pages/ApproveAccounts';
-=======
 import AddGrupo from './Pages/Grupos';
 import Grupos from './Pages/Grupos';
->>>>>>> 478947e895b3f2cb56edf16421567151c30d83e3
 
 function App() {
   return (
@@ -25,10 +22,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/proyectos" element={<Proyectos />} />
         <Route path="/grupo" element={<Grupos />} />
-        <Route path="/register" element={<Register />} />  {/* Nueva ruta para Register */}
-        <Route path="/forgot-password" element={<ForgotPassword />} />  {/* Nueva ruta para Forgot Password */}
-        <Route path="/proyecto-estudiante" element={<ProyectoEstudiante />} />   {/* Nueva ruta para ProyectoEstudiante */}
-        <Route path="/planificacion-estudiante" element={<PlanificacionEstudiante />} />   {/* Nueva ruta para PlanificacionEstudiante */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/proyecto-estudiante" element={<ProyectoEstudiante />} />
+        <Route path="/planificacion-estudiante" element={<PlanificacionEstudiante />} />
         <Route path="/historia-usuario/:id" element={<HistoriaUsuario />} />
         <Route path="/perfil" element={<Perfil />} />
         <Route path="/approve-accounts" element={<ApproveAccounts />} />
